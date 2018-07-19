0e56552d727b86f1e45d5aa086955e09812ad326
eastcirclek <eastcirclek@gmail.com>
2018-07-19 16:54:27 +0900

    first commit

---
+++ src/main/scala/com/github/eastcirclek/CountWindow.scala
@@ -0,0 +1,24 @@
+package com.github.eastcirclek
+
+import org.apache.flink.api.scala._
+import org.apache.flink.streaming.api.scala.StreamExecutionEnvironment
+import org.apache.flink.util.Collector
+
+object CountWindow {
+  def main(args: Array[String]): Unit = {
+
+    val env = StreamExecutionEnvironment.getExecutionEnvironment
+    env.setParallelism(1)
+
+    env
+      .fromElements(1 to 6:_*)
+      .countWindowAll(3, 2)
+      .apply {
+        (_, iterator, collector: Collector[String]) =>
+          collector.collect(iterator.mkString(","))
+      }
+      .print()
+
+    env.execute()
+  }
+}
