6e30ad7f770902fc010feda4f6d506cfbdb1b141
eastcirclek <eastcirclek@gmail.com>
2018-08-26 23:16:30 +0900

    Do not re-emit results if there's no change in the window content

--- src/main/scala/com/github/eastcirclek/examples/window/SessionWindowWithEarlyResultTrigger.scala
+++ src/main/scala/com/github/eastcirclek/examples/window/SessionWindowWithEarlyResultTrigger.scala
@@ -1,29 +1,37 @@
-package com.github.eastcirclek.examples.window.trigger
+package com.github.eastcirclek.examples.window
 
 import com.github.eastcirclek.examples.{MyRecord, MyWatermark, StreamElement}
-import com.github.eastcirclek.flink.trigger.DelayedEarlyResultEventTimeTrigger
+import com.github.eastcirclek.flink.trigger.EarlyResultEventTimeTrigger
 import org.apache.flink.api.scala._
 import org.apache.flink.streaming.api.TimeCharacteristic
 import org.apache.flink.streaming.api.functions.source.SourceFunction.SourceContext
-import org.apache.flink.streaming.api.functions.timestamps.BoundedOutOfOrdernessTimestampExtractor
 import org.apache.flink.streaming.api.scala.StreamExecutionEnvironment
 import org.apache.flink.streaming.api.watermark.Watermark
 import org.apache.flink.streaming.api.windowing.assigners.EventTimeSessionWindows
 import org.apache.flink.streaming.api.windowing.time.Time._
 import org.apache.flink.util.Collector
 
-object SessionWindowWithDelayedEarlyResultTrigger2 {
+object SessionWindowWithEarlyResultTrigger {
   def main(args: Array[String]): Unit = {
     val records = Seq[StreamElement](
       MyRecord('a', 2),
       MyRecord('b', 4),
       MyRecord('c', 6, true),
+      MyRecord('g', 14),
+      MyRecord('h', 16),
+      MyRecord('i', 18, true),
       MyRecord('d', 8),
       MyRecord('e', 10),
       MyRecord('f', 12, true),
-      MyRecord('g', 14),
-      MyRecord('h', 16),
-      MyRecord('i', 18, true)
+      MyWatermark(12),
+      MyWatermark(18),
+      MyWatermark(22),
+      MyRecord('j', 30),
+      MyRecord('l', 34, true),
+      MyRecord('k', 32),
+      MyWatermark(34),
+      MyRecord('m', 35),
+      MyWatermark(42)
     )
 
     val env = StreamExecutionEnvironment.getExecutionEnvironment
@@ -43,26 +51,17 @@ object SessionWindowWithDelayedEarlyResultTrigger2 {
             } else {
               println(s"$value @ $timestamp")
             }
-
             context.collectWithTimestamp(record, timestamp)
             Thread.sleep(200)
         }
       )
       .map(rec => (1, rec))
-      .assignTimestampsAndWatermarks(
-        new BoundedOutOfOrdernessTimestampExtractor[(Int, MyRecord)](milliseconds(2)) {
-          override def extractTimestamp(tp: (Int, MyRecord)): Long = {
-            val element = tp._2
-            element.timestamp
-          }
-        }
-      )
       .keyBy(0)
       .window(EventTimeSessionWindows.withGap(milliseconds(5)))
-      .trigger(new DelayedEarlyResultEventTimeTrigger[(Int, MyRecord)](_._2.last))
+      .trigger(new EarlyResultEventTimeTrigger[(Int, MyRecord)](_._2.last))
       .apply(
         (key, window, iterator, collector: Collector[String]) =>
-          collector.collect(window.toString + " : " + iterator.map(_._2).mkString(", "))
+          collector.collect("[apply] " + window.toString + " : " + iterator.map(_._2).mkString(", "))
       )
       .print()
 
--- src/main/scala/com/github/eastcirclek/flink/trigger/EarlyResultEventTimeTrigger.scala
+++ src/main/scala/com/github/eastcirclek/flink/trigger/EarlyResultEventTimeTrigger.scala
@@ -1,29 +1,66 @@
 package com.github.eastcirclek.flink.trigger
 
+import org.apache.flink.api.common.functions.AggregateFunction
+import org.apache.flink.api.common.state.{AggregatingStateDescriptor, ListStateDescriptor}
 import org.apache.flink.streaming.api.windowing.triggers.{Trigger, TriggerResult}
 import org.apache.flink.streaming.api.windowing.windows.TimeWindow
 
+import scala.collection.JavaConverters._
+
 class EarlyResultEventTimeTrigger[T](eval: (T => Boolean)) extends Trigger[T, TimeWindow] {
-  override def onElement(element: T, timestamp: Long, window: TimeWindow, ctx: Trigger.TriggerContext): TriggerResult = {
-    if (window.maxTimestamp <= ctx.getCurrentWatermark) {
-      println(s"[onElement] $window - FIRE (allowed lateness)")
+  val timersDesc = new ListStateDescriptor[Long]("timersAcrossMerges", classOf[Long])
+  val countDesc = new AggregatingStateDescriptor("count", LongAdder.create(), classOf[Long])
+  val lastCountWhenFiringDesc = new AggregatingStateDescriptor("countWhenFiring", LongAdder.create(), classOf[Long])
+
+  def fireOrContinue(ctx: Trigger.TriggerContext): TriggerResult = {
+    val count_val = ctx.getPartitionedState(countDesc).get
+    val lastCount = ctx.getPartitionedState(lastCountWhenFiringDesc)
+    val lastCount_val = lastCount.get
+    val diff = count_val - lastCount_val
+    lastCount.add(diff)
+
+    val result = if (diff > 0) {
       TriggerResult.FIRE
     } else {
+      TriggerResult.CONTINUE
+    }
+    println(s"[fireOrContinue] count_${count_val} lastCount_${lastCount_val} $result")
+
+    result
+  }
+
+  override def onElement(element: T, timestamp: Long, window: TimeWindow, ctx: Trigger.TriggerContext): TriggerResult = {
+    ctx.getPartitionedState(countDesc).add(1)
+
+    if (window.maxTimestamp <= ctx.getCurrentWatermark) {
+      println(s"[onElement] $window $element - FIRE (allowed lateness)")
+      fireOrContinue(ctx)
+    } else {
       if (eval(element)) {
-        println(s"[onElement] $window - FIRE (early emit)")
-        TriggerResult.FIRE_AND_PURGE
-      } else {
-        println(s"[onElement] $window registerTimer_${window.maxTimestamp} - CONTINUE")
-        ctx.registerEventTimeTimer(window.maxTimestamp)
-        TriggerResult.CONTINUE
+        val timers = ctx.getPartitionedState(timersDesc)
+        timers.add(timestamp)
+        println(s"[onElement] $window $element registerTimer_$timestamp for early fire")
+        ctx.registerEventTimeTimer(timestamp)
       }
+      println(s"[onElement] $window $element registerTimer_${window.maxTimestamp} - CONTINUE")
+      ctx.registerEventTimeTimer(window.maxTimestamp)
+      TriggerResult.CONTINUE
     }
   }
 
   override def onEventTime(time: Long, window: TimeWindow, ctx: Trigger.TriggerContext): TriggerResult = {
-    if (time == window.maxTimestamp) {
-      println(s"[onEventTime] $window time_$time - FIRE (event time)")
-      TriggerResult.FIRE
+    if (time < window.maxTimestamp) {
+      println(s"[onEventTime] $window time_$time - FIRE (registered event time)")
+      println(s"[onEventTime] $window deleteTimer_$time")
+      ctx.deleteEventTimeTimer(time)
+
+      val timers = ctx.getPartitionedState(timersDesc)
+      timers.update(timers.get.asScala.filter(_ != time).toSeq.asJava)
+
+      fireOrContinue(ctx)
+    } else if (time == window.maxTimestamp) {
+      println(s"[onEventTime] $window time_$time - FIRE (maxtimestamp)")
+      fireOrContinue(ctx)
     } else {
       println(s"[onEventTime] $window time_$time - CONTINUE")
       TriggerResult.CONTINUE
@@ -42,9 +79,32 @@ class EarlyResultEventTimeTrigger[T](eval: (T => Boolean)) extends Trigger[T, Ti
   override def canMerge: Boolean = true
 
   override def onMerge(window: TimeWindow, ctx: Trigger.OnMergeContext): Unit = {
+    ctx.mergePartitionedState(countDesc)
+    ctx.mergePartitionedState(lastCountWhenFiringDesc)
+    ctx.mergePartitionedState(timersDesc)
+
+    val timers = ctx.getPartitionedState(timersDesc)
+    if (timers.get != null) {
+      timers.get.asScala.foreach { timestamp =>
+        println(s"[onMerge] $window restoreTimer_$timestamp")
+        ctx.registerEventTimeTimer(timestamp)
+      }
+    }
+
     println(s"[onMerge] $window registerTimer_${window.maxTimestamp}")
     ctx.registerEventTimeTimer(window.maxTimestamp)
   }
 
-  override def toString = "EarlyResultEventTimeTrigger()"
+  override def toString = "DelayedEarlyResultEventTimeTrigger()"
+}
+
+object LongAdder {
+  def create(): AggregateFunction[Long, Long, Long] = {
+    new AggregateFunction[Long, Long, Long] {
+      override def add(n: Long, acc: Long): Long = n + acc
+      override def createAccumulator(): Long = 0
+      override def getResult(acc: Long): Long = acc
+      override def merge(n1: Long, n2: Long): Long = n1 + n2
+    }
+  }
 }
